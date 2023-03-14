0a44eeae2722bb3d888fa3709d258f142f2eb4a8
jacobvjk <jacob.kastl@gmail.com>
2023-03-14 12:09:01 +0100

    placeholders energy efficiency

--- data-raw/primary_energy_efficiency.R
+++ data-raw/primary_energy_efficiency.R
@@ -3,21 +3,18 @@
 # energy. To back calculate the primary energy content based on electricity
 # metrics, such as power generation, therefore requires dividing the power
 # capacity by this efficiency factor to derive the physical energy input.
-# In essence, this means that a primary energy equivalent (potentially) higher
-# than the power capacity (which refers to electricity only) was used as input
-# for a given quantity of generated electricity.
+# We apply such factors for fossil fuel based power generation only, since we
+# are interested in approximating the exposure to fossil fuels.
 
 # TODO: values are approximate placeholders and need updating
 primary_energy_efficiency <- tibble::tribble(
   ~sector,     ~technology, ~primary_energy_efficiency_factor,
-  "power",       "coalcap",             0.30,
-  "power",        "gascap",             0.30,
-  "power",        "oilcap",             0.30,
-  # no combustion/heat
-  "power",      "hydrocap",             1,
-  "power",    "nuclearcap",             0.30,
-  # some renewables (wind, marine, solar PV) use no combustion/heat
-  "power", "renewablescap",             0.60
+  "power",       "coalcap",                              0.30,
+  "power",        "gascap",                              0.30,
+  "power",        "oilcap",                              0.30,
+  "power",      "hydrocap",                                 1,
+  "power",    "nuclearcap",                                 1,
+  "power", "renewablescap",                                 1
 )
 
 usethis::use_data(primary_energy_efficiency, overwrite = TRUE)
