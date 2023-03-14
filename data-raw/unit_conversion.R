56c46e5efbbfff1fb1f45811d99ad1f4c0f278f2
jacobvjk <jacob.kastl@gmail.com>
2023-03-14 12:09:20 +0100

    fix unit in conversion table

--- data-raw/unit_conversion.R
+++ data-raw/unit_conversion.R
@@ -4,7 +4,7 @@
 unit_conversion <- tibble::tribble(
   ~sector,       ~unit,             ~value_in_mtoe,
   # TODO: get the tech type conversions for coal
-  "coal",        "tonnes per year", 7e-07,
+  "coal",        "t coal",          7e-07,
   "oil and gas", "GJ",              2.3885e-08,
   "power",       "MWh",             8.598e-08
 )
