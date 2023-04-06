8224c29db43f122605f59b4f98d8c0cdf495799a
MonikaFu <monika.furdyna@gmail.com>
2023-04-06 13:45:58 +0200

    Refine and document

--- R/plot_scatter_animated.R
+++ R/plot_scatter_animated.R
@@ -1,11 +1,110 @@
-plot_scatter_animated <- function(data, sector = NULL) {
+#' Plot alignment scatterplot
+#'
+#' @param data data.frame. Should have the same format as output of
+#'   `prep_scatter_animated()` and contain columns: 'name', 'buildout',
+#'   phaseout', 'net' and 'year'.
+#' @param data_level Character. Level of the plotted data. Can be 'bank' or
+#'   'company'.
+#' @param sector Character. Sector name to be used in the plot title.
+#' @param scenario_source Character. Scenario source to be used in the plot
+#'   caption.
+#' @param scenario Character. Scenario name to be used in the plot caption.
+#' @param region Character. Region to be used in the plot caption.
+#' @param title Character. Custom title if different than default.
+#' @param subtitle Character. Custom subtitle if different than default.
+#' @param alignment_limit Numeric. Limit to be applied to the x- and y-axis
+#'   scales and to alignment values for colouring. By default the maximum
+#'   absolute alignment value of is used.
+#' @param cap_outliers Numeric. Cap which should be applied to the alignment
+#'   values in the data. Values bigger than cap are plotted on the border of the
+#'   plot.
+#' @param floor_outliers Numeric. Floor which should be applied to the alignment
+#'   values in the data. Values smaller than floor are plotted on the border of
+#'   the plot.
+#'
+#' @return object of type "plotly"
+#' @export
+#'
+#' @examples
+#' #TODO
+plot_scatter_animated <- function(
+    data,
+    data_level = c("company", "bank"),
+    sector = NULL,
+    scenario_source = NULL,
+    scenario = NULL,
+    region = NULL,
+    title = NULL,
+    subtitle = NULL,
+    alignment_limit = NULL,
+    cap_outliers = NULL,
+    floor_outliers = NULL
+    ) {
+  arg_match(data_level)
 
-  alignment_limit <- max(abs(c(data$buildout, data$phaseout, data$net)), na.rm = TRUE)
+  caption <- ""
+  if (!is.null(scenario_source) | !is.null(scenario) | !is.null(region)) {
+    if (!is.null(scenario)) {
+      caption <- glue("Scenario: {beautify_scenario_label(scenario)}\n", .trim = FALSE)
+    }
+    if (!is.null(scenario_source)) {
+      caption <- glue("{caption}Scenario source: {beautify_scenario_label(scenario_source)}\n", .trim = FALSE)
+    }
+    if(!is.null(region)) {
+      caption <- glue("{caption}Region: {r2dii.plot::to_title(region)}", .trim = FALSE)
+    }
+  } else {
+    rlang::warn("No information to display in caption provided. Please provide scenario_source and/or scenario and/or region if you want them to be included in the graph", frequency = "once")
+  }
 
-  subtitle <- "Each dot is a company. The companies in the top right quadrant are both building out\nlow-carbon technologies and phasing out high-carbon technologies at rates\ngreater or equal to those required by the scenario."
-  title <- glue("<b>Build-out vs. Phase-out Alignment in the {r2dii.plot::to_title(sector)} Sector</b>\n\n<sup>{subtitle}</sup>")
+  if (is.null(title)) {
+    if (!is.null(sector)) {
+      title <- glue("Build-out vs. Phase-out Alignment \nin the {r2dii.plot::to_title(sector)} Sector")
+    } else {
+      title <- "Build-out vs. Phase-out Alignment"
+    }
+  }
 
-  caption <- "Scenario: X\nScenario source: Y\nRegion: Z"
+  if (data_level == "company") {
+    title <- paste0(title, " per Company")
+    if (is.null(subtitle)) {
+      subtitle <- "Each dot is a company. The companies in the top right quadrant are both building out\n low-carbon technologies and phasing out high-carbon technologies at rates\ngreater or equal to those required by the scenario."
+    }
+  } else {
+    title <- paste0(title, " per Bank")
+    if (is.null(subtitle)) {
+      subtitle <- "Each dot is a bank. The banks in the top right quadrant are exposed to companies\nwhich on aggregate level are both building out low-carbon technologies and phasing out\nhigh-carbon technologies at rates greater or equal to those required by the scenario."
+    }
+  }
+
+  check_plot_scatter_animated(data, alignment_limit, cap_outliers, floor_outliers)
+
+  if(!is.null(floor_outliers)) {
+    data <- data %>%
+      mutate(
+        buildout = if_else(.data$buildout <= .env$floor_outliers, .env$floor_outliers, .data$buildout),
+        phaseout = if_else(.data$phaseout <= .env$floor_outliers, .env$floor_outliers, .data$phaseout),
+        net = if_else(.data$net <= .env$floor_outliers, .env$floor_outliers, .data$net)
+      )
+    subtitle <- glue("{subtitle}\nThe outliers are displayed on the borders of the plot.", .trim = FALSE)
+  }
+  if (!is.null(cap_outliers)) {
+    data <- data %>%
+      mutate(
+        buildout = if_else(.data$buildout >= .env$cap_outliers, .env$cap_outliers, .data$buildout),
+        phaseout = if_else(.data$phaseout >= .env$cap_outliers, .env$cap_outliers, .data$phaseout),
+        net = if_else(.data$net >= .env$cap_outliers, .env$cap_outliers, .data$net)
+      )
+    if (is.null(floor_outliers)) {
+      subtitle <- glue("{subtitle}\nThe outliers are displayed on the borders of the plot.", .trim = FALSE)
+    }
+  }
+
+  title <- glue("<b>{title}</b>\n\n<sup>{subtitle}</sup>")
+
+  if (is.null(alignment_limit)) {
+    alignment_limit <- max(abs(c(data$buildout, data$phaseout, data$net)), na.rm = TRUE)
+  }
 
   p <- plotly::plot_ly(
       x = ~buildout,
@@ -74,7 +173,7 @@ plot_scatter_animated <- function(data, sector = NULL) {
       xanchor = "right",
       align = "right",
       xref = "paper",
-      y = -0.7,
+      y = -0.63,
       yref = "paper",
       showarrow = F
     ) %>%
@@ -115,7 +214,7 @@ plot_scatter_animated <- function(data, sector = NULL) {
         tickformat = ",.0%"
         ),
       plot_bgcolor = "#6c6c6c",
-      margin = list(l = 0, r = 0, t = 125, b = 250),
+      margin = list(l = 0, r = 0, t = 155, b = 250),
       shapes = list(
         list(
           type = "line",
@@ -138,4 +237,29 @@ plot_scatter_animated <- function(data, sector = NULL) {
   p$x$layout$updatemenus[[1]]$pad$t <- 80
 
   p
+}
+
+check_plot_scatter_animated <- function(
+    data,
+    alignment_limit,
+    cap_outliers,
+    floor_outliers
+    ) {
+  r2dii.plot:::abort_if_missing_names(data, c("name", "buildout",
+   "phaseout", "net", "year"))
+  if (!is.null(alignment_limit)) {
+    if ((length(alignment_limit) != 1) | (!is.numeric(alignment_limit))){
+      rlang::abort("'alignment_limit' must be a numeric value.")
+    }
+  }
+  if (!is.null(cap_outliers)) {
+    if ((length(cap_outliers) != 1) | (!is.numeric(cap_outliers))){
+     rlang::abort("'cap_outliers' must be a numeric value.")
+    }
+  }
+  if (!is.null(floor_outliers)) {
+    if ((length(floor_outliers) != 1) | (!is.numeric(floor_outliers))){
+      rlang::abort("'floor_outliers' must be a numeric value.")
+    }
+  }
 }
--- R/prep_scatter_animated.R
+++ R/prep_scatter_animated.R
@@ -1,3 +1,23 @@
+#' Prepare data to plot animated scatterplot
+#'
+#' @param data_bopo data.frame. Data containing buildout and phaseout alignment
+#'   values. Must contain columns: 'bank_id', 'year', 'sector', 'region',
+#'   'direction' and either 'name_abcd' and 'score' or
+#'   'exposure_weighted_net_alignment'.
+#' @param data_net data.frame. Data containing net alignment values. Must
+#'   contain columns: 'bank_id', 'year', 'sector', 'region', 'direction' and
+#'   either 'name_abcd' and 'score' or 'exposure_weighted_net_alignment'.
+#' @param data_level Character. Level of the plotted data. Can be 'bank' or
+#'   'company'.
+#' @param sector Character. Sector to filter data on.
+#' @param region Character. Region to filter data on.
+#' @param bank_ids_to_plot Character vector. Bank ids to filter on.
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' #TODO
 prep_scatter_animated <- function(
     data_bopo,
     data_net,
@@ -6,6 +26,7 @@ prep_scatter_animated <- function(
     region,
     bank_ids_to_plot = NULL
     ) {
+  arg_match(data_level)
 
   if (data_level == "bank") {
     name_col = "bank_id"
@@ -14,6 +35,9 @@ prep_scatter_animated <- function(
     name_col = "name_abcd"
     value_col = "score"
   }
+
+  check_prep_scatter_animated(data_bopo, sector, region, bank_ids_to_plot, name_col, value_col)
+  check_prep_scatter_animated(data_net, sector, region, bank_ids_to_plot, name_col, value_col)
 
   if (is.null(bank_ids_to_plot)) {
     bank_ids_to_plot <- unique(c(data_bopo$bank_id, data_net$bank_id))
@@ -38,4 +62,19 @@ prep_scatter_animated <- function(
       )
     )
   data_scatter
+}
+
+check_prep_scatter_animated <- function(
+    data,
+    sector,
+    region,
+    bank_ids_to_plot,
+    name_col,
+    value_col
+    ) {
+  r2dii.plot:::abort_if_missing_names(data, c("bank_id", "year",
+   "sector", "region", "direction", name_col, value_col))
+  r2dii.plot:::abort_if_unknown_values(sector, data, "sector")
+  r2dii.plot:::abort_if_unknown_values(region, data, "region")
+  r2dii.plot:::abort_if_unknown_values(bank_ids_to_plot, data, "bank_id")
 }
