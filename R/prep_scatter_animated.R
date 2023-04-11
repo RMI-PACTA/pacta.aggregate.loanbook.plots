c1448afade6e6b97efc8d8bf71311f7ce8a11308
MonikaFu <monika.furdyna@gmail.com>
2023-04-11 16:03:59 +0200

    Adjust to new names

--- R/prep_scatter_animated.R
+++ R/prep_scatter_animated.R
@@ -1,17 +1,17 @@
 #' Prepare data to plot animated scatterplot
 #'
 #' @param data_bopo data.frame. Data containing buildout and phaseout alignment
-#'   values. Must contain columns: 'bank_id', 'year', 'sector', 'region',
-#'   'direction' and either 'name_abcd' and 'score' or
+#'   values. Must contain columns: 'group_id', 'year', 'sector', 'region',
+#'   'direction' and either 'name_abcd' and 'alignment_metric' or
 #'   'exposure_weighted_net_alignment'.
 #' @param data_net data.frame. Data containing net alignment values. Must
-#'   contain columns: 'bank_id', 'year', 'sector', 'region', 'direction' and
-#'   either 'name_abcd' and 'score' or 'exposure_weighted_net_alignment'.
+#'   contain columns: 'group_id', 'year', 'sector', 'region', 'direction' and
+#'   either 'name_abcd' and 'alignment_metric' or 'exposure_weighted_net_alignment'.
 #' @param data_level Character. Level of the plotted data. Can be 'bank' or
 #'   'company'.
 #' @param sector Character. Sector to filter data on.
 #' @param region Character. Region to filter data on.
-#' @param bank_ids_to_plot Character vector. Bank ids to filter on.
+#' @param group_ids_to_plot Character vector. Group ids to filter on.
 #'
 #' @return data.frame
 #' @export
@@ -24,23 +24,23 @@ prep_scatter_animated <- function(
     data_level = c("bank", "company"),
     sector,
     region,
-    bank_ids_to_plot = NULL
+    group_ids_to_plot = NULL
     ) {
   arg_match(data_level)
 
   if (data_level == "bank") {
-    name_col = "bank_id"
+    name_col = "group_id"
     value_col = "exposure_weighted_net_alignment"
   } else {
     name_col = "name_abcd"
-    value_col = "score"
+    value_col = "alignment_metric"
   }
 
-  check_prep_scatter_animated(data_bopo, sector, region, bank_ids_to_plot, name_col, value_col)
-  check_prep_scatter_animated(data_net, sector, region, bank_ids_to_plot, name_col, value_col)
+  check_prep_scatter_animated(data_bopo, sector, region, group_ids_to_plot, name_col, value_col)
+  check_prep_scatter_animated(data_net, sector, region, group_ids_to_plot, name_col, value_col)
 
-  if (is.null(bank_ids_to_plot)) {
-    bank_ids_to_plot <- unique(c(data_bopo$bank_id, data_net$bank_id))
+  if (is.null(group_ids_to_plot)) {
+    group_ids_to_plot <- unique(c(data_bopo$group_id, data_net$group_id))
   }
 
   data_scatter <- data_bopo %>%
@@ -48,7 +48,7 @@ prep_scatter_animated <- function(
     filter(
       .data$sector == .env$sector,
       .data$region == .env$region,
-      .data$bank_id %in% bank_ids_to_plot
+      .data$group_id %in% group_ids_to_plot
       ) %>%
     select("name" = name_col, "direction", "year", "value" = value_col) %>%
     distinct() %>%
@@ -72,13 +72,13 @@ check_prep_scatter_animated <- function(
     data,
     sector,
     region,
-    bank_ids_to_plot,
+    group_ids_to_plot,
     name_col,
     value_col
     ) {
-  r2dii.plot:::abort_if_missing_names(data, c("bank_id", "year",
+  r2dii.plot:::abort_if_missing_names(data, c("group_id", "year",
    "sector", "region", "direction", name_col, value_col))
   r2dii.plot:::abort_if_unknown_values(sector, data, "sector")
   r2dii.plot:::abort_if_unknown_values(region, data, "region")
-  r2dii.plot:::abort_if_unknown_values(bank_ids_to_plot, data, "bank_id")
+  r2dii.plot:::abort_if_unknown_values(group_ids_to_plot, data, "group_id")
 }
