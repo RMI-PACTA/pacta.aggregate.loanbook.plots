e427c4687eed3fbec1823d171c8a7739d7249afd
jacobvjk <jacob.kastl@gmail.com>
2023-08-21 09:45:25 +0200

    simplify

--- R/aggregate_alignment_loanbook_exposure.R
+++ R/aggregate_alignment_loanbook_exposure.R
@@ -2,15 +2,24 @@
 #'
 #' @param data data.frame. Holds output pf company indicators
 #' @param matched data.frame. Holds matched and prioritised loan book
+#' @param level Character. Vector that indicates if the aggreagte alignment
+#'   metric should be returned based on the net technology deviations (`net`) or
+#'   disaggregated into buildout and phaseout technologies (`bo_po`).
 #'
 #' @return NULL
 #' @export
 aggregate_alignment_loanbook_exposure <- function(data,
-                                                  matched) {
+                                                  matched,
+                                                  level = c("net", "bo_po")) {
+
+  group_vars <- c("group_id", "scenario", "region", "sector", "year", "direction")
+  level <- match.arg(level)
+
   # validate input data sets
   validate_input_data_aggregate_alignment_loanbook_exposure(
     data = data,
-    matched = matched
+    matched = matched,
+    group_vars = group_vars
   )
 
   matched <- matched %>%
@@ -38,7 +47,7 @@ aggregate_alignment_loanbook_exposure <- function(data,
     dplyr::mutate(
       n_companies = dplyr::n(),
       sum_loan_size_outstanding = sum(.data$loan_size_outstanding, na.rm = TRUE),
-      .by = c("group_id", "region", "scenario", "sector", "year", "direction")
+      .by = group_vars
     ) %>%
     dplyr::mutate(
       companies_aligned = dplyr::if_else(.data$alignment_metric >= 0, TRUE, FALSE),
@@ -47,10 +56,7 @@ aggregate_alignment_loanbook_exposure <- function(data,
     dplyr::summarise(
       n_companies_aligned = sum(.data$companies_aligned, na.rm = TRUE),
       sum_exposure_companies_aligned = sum(.data$exposure_companies_aligned, na.rm = TRUE),
-      .by = c(
-        "group_id", "n_companies", "sum_loan_size_outstanding",
-        "scenario", "region", "sector", "year", "direction"
-      )
+      .by = c(group_vars, "n_companies", "sum_loan_size_outstanding")
     ) %>%
     dplyr::mutate(
       share_companies_aligned = .data$n_companies_aligned / .data$n_companies,
@@ -60,13 +66,13 @@ aggregate_alignment_loanbook_exposure <- function(data,
   # if a company only has technologies going in one direction in a sector with
   # high carbon and low carbon technologies, add an empty entry for the other
   # direction to ensure the aggregation is correct
-  if (all(unique(aggregate_exposure_company$direction) %in% c("buildout", "phaseout"))) {
+  if (level == "bo_po") {
     aggregate_exposure_company <- aggregate_exposure_company %>%
       dplyr::mutate(
         n_directions = dplyr::n(),
         .by = c(
-          "group_id", "name_abcd", "sector", "activity_unit", "region",
-          "scenario_source", "scenario", "year", "loan_size_outstanding_currency"
+          group_vars[!group_vars == "direction"], "name_abcd", "sector",
+          "activity_unit", "loan_size_outstanding_currency"
         )
       )
 
@@ -96,20 +102,17 @@ aggregate_alignment_loanbook_exposure <- function(data,
   sector_aggregate_exposure_loanbook_alignment <- aggregate_exposure_company %>%
     dplyr::summarise(
       exposure_weighted_net_alignment = stats::weighted.mean(.data$alignment_metric, w = .data$exposure_weight, na.rm = TRUE),
-      .by = c(
-        "group_id", "scenario", "region", "sector", "year", "direction"
-      )
+      .by = group_vars
     )
 
   out <- sector_aggregate_exposure_loanbook_summary %>%
     dplyr::inner_join(
       sector_aggregate_exposure_loanbook_alignment,
-      by = c("group_id", "scenario", "region", "sector", "year", "direction")
+      by = group_vars
     ) %>%
     dplyr::relocate(
       c(
-        "group_id", "scenario", "region", "sector", "year", "direction",
-        "n_companies", "n_companies_aligned", "share_companies_aligned",
+        group_vars, "n_companies", "n_companies_aligned", "share_companies_aligned",
         "sum_loan_size_outstanding", "sum_exposure_companies_aligned",
         "share_exposure_aligned", "exposure_weighted_net_alignment"
       )
@@ -120,12 +123,12 @@ aggregate_alignment_loanbook_exposure <- function(data,
 }
 
 validate_input_data_aggregate_alignment_loanbook_exposure <- function(data,
-                                                                      matched) {
+                                                                      matched,
+                                                                      group_vars) {
   validate_data_has_expected_cols(
     data = data,
     expected_columns <- c(
-      "group_id", "name_abcd", "sector", "activity_unit", "region",
-      "scenario_source", "scenario", "year", "direction", "alignment_metric"
+      group_vars, "name_abcd", "activity_unit", "scenario_source", "alignment_metric"
     )
   )
 
